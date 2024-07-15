94efc703045efd77f4e445dbed607efd42cef2b3
Srigadhakeerthi <srigadha.keerthi7989@gmail.com>
2024-07-16 00:22:15 +0530

    Add files via upload

---
+++ E-Learning website/script.js
@@ -0,0 +1,9 @@
+// Placeholder for future JavaScript functionalities
+
+// Dropdown Menu Toggle
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+    dropdown.addEventListener('mouseover', function() {
+        this.querySelector('.dropdown-content').style.display = 'block';
+    });
+    dropdown.addEventListener('mouseout', function() {
+        this.querySelector('.dropdown
